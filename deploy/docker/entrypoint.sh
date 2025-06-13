5b17823955059288bef0c1035c778a79407566e6
Kareem Shousha <kemoshousha309@gmail.com>
2025-06-13 08:39:33 +0300

    Fix

--- deploy/docker/entrypoint.sh
+++ deploy/docker/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "Running entrypoint.sh ..."
 
-python3 ./license/main.py
+python3 ./deploy/license-verification/src/main.py
 exit_code=$?
 if [ $exit_code -ne 0 ]; then
     exit $exit_code
 fi
 
-python3 ./license/job.py &
+python3 ./deploy/license-verification/src/job.py &
 
 exec odoo "$@"
 
