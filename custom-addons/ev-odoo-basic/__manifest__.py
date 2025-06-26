1d94d4ac953890dafcfdd2c5f67ba819b41f1f7c
Kareem Shousha <kemoshousha309@gmail.com>
2025-06-26 18:03:06 +0300

    feat: init basic odoo structure

---
+++ custom-addons/ev-odoo-basic/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    'name': 'Experts Vision',
+    'version': '18.0.1.0.0',
+    'category': 'Genral',
+    'summary': 'Experts Vision ERP starter',
+    'description': """
+        This module provides features for managing basic ERPs in Odoo.
+    """,
+    'author': 'Experts Vision Company',
+    'website': 'https://www.expertsvision.com',
+    'depends': ['base', 'stock', 'purchase','sale_management'],
+    'data': [],
+    'demo': [],
+    'application': False,
+    'license': 'AGPL-3',
+    'images': ['static/description/icon.png'],
+    'support': 'support@expertsvision.com',
+
+    # 'installable': True,
+    # 'auto_install': False,
+    # 'currency': 'EUR',
+    # 'maintainer': 'Maintainer Name',
+    # 'external_dependencies': {
+    #     'python': [],  # Python libraries required
+    #     'bin': [],  # External binaries required
+    # },
+    # 'pre_init_hook': 'pre_init_hook_method',  # Hook to execute before module installation
+    # 'post_init_hook': 'post_init_hook_method',  # Hook to execute after module installation
+    # 'post_load_hook': 'post_load_hook_method',  # Hook to execute after loading the module
+}
